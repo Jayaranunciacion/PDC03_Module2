87d7d4d350e3b22da2bcbb390c5dfe078fe19e7e
akceberyd <mallari.sharonwendy@auf.edu.ph>
2023-03-03 10:17:33 +0800

    first commit

---
+++ PDC03_MODULE2/activity5.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PDC03_MODULE2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity5 : ContentPage
+    {
+        ObservableCollection<employee> employees = new ObservableCollection<employee>();
+        public ObservableCollection<employee> Employees { get { return employees; } }
+
+
+        public activity5()
+        {
+            InitializeComponent();
+
+
+            {
+                lst.ItemsSource = employees;
+
+                employees.Add(new employee { DisplayNameAttribute = "Juan Dela Cruz", Position = "President", profileimage = "image1.jpg" });
+                employees.Add(new employee { DisplayNameAttribute = "Peter", Position = "Vice President", profileimage = "image2.jpg" });
+                employees.Add(new employee { DisplayNameAttribute = "Mary", Position = "Secretary", profileimage = "image3.jpg" });
+            }
+        }
+    }
+}
